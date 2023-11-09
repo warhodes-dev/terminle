c21f4e4b392ecb840034eb86aa56fa06a10c42f0
warhodes-dev <warhodes@gmail.com>
2023-11-09 12:58:02 -0500

    wurdle done

--- src/main.rs
+++ src/main.rs
@@ -29,85 +29,148 @@ fn main() {
     }
 }
 
+#[derive(Clone, Copy)]
+enum Status {
+    InvalidWord,
+    InvalidLength,
+    AlreadyGuessed,
+}
+
+impl Status {
+    fn msg(&self) -> &str {
+        match self {
+            Status::InvalidWord => "✗ Invalid word",
+            Status::InvalidLength => "✗ Must be 5 characters long",
+            Status::AlreadyGuessed => "✗ Already guessed",
+        }
+    }
+}
+
 fn app(mut wordle: Wordle) -> Result<()> {
     
     terminal::enable_raw_mode()?;
 
     let mut guess = String::new();
+    let mut status: Option<Status> = None;
     loop {
         if let Event::Key(KeyEvent { code, modifiers, .. }) = event::read()? {
 
+            if status.is_some() {
+                clear_status()?;
+            }
+
             match (code, modifiers) {
                 (KeyCode::Char('c'), KeyModifiers::CONTROL) | (KeyCode::Esc, _) => break,
-                (KeyCode::Char(c), _) => {
-                    if guess.len() == 5 {
-                        continue;
-                    }
-
-                    guess.push(c);
-                    let hint = wordle.hint(&c, guess.len() - 1);
-                    let color = match hint {
-                        None => style::Color::Reset,
-                        Some(Hint::Green(_)) => style::Color::Green,
-                        Some(Hint::Yellow) => style::Color::Yellow,
-                        Some(Hint::Gray) => style::Color::Grey,
-                    };
-                    io::stdout()
-                        .execute(style::SetForegroundColor(color))?
-                        .execute(style::Print(c))?;
-                },
-                (KeyCode::Backspace, _) => {
-                    if guess.len() == 0 {
-                        continue;
-                    }
-
-                    guess.pop();
-                    io::stdout()
-                        .execute(cursor::MoveLeft(1))?
-                        .execute(style::Print(' '))?
-                        .execute(cursor::MoveLeft(1))?;
-                },
-                (KeyCode::Enter, _) => {
-                    if guess.len() != 5 {
-                        continue;
-                    }
-
-                    let result = wordle.guess(&guess);
-
-                    io::stdout()
-                        .execute(cursor::MoveToColumn(0))?;
-
-                    for (c, hint) in result {
-                        let (fg, bg) = match hint {
-                            Hint::Green(_) => (style::Color::Black, style::Color::DarkGreen),
-                            Hint::Yellow => (style::Color::Black, style::Color::DarkYellow),
-                            Hint::Gray => (style::Color::White, style::Color::DarkGrey),
-                        };
-                        io::stdout()
-                            .execute(style::SetForegroundColor(fg))?
-                            .execute(style::SetBackgroundColor(bg))?
-                            .execute(style::Print(c))?;
-                    }
-
-                    io::stdout()
-                        .execute(style::SetForegroundColor(style::Color::Reset))?
-                        .execute(style::SetBackgroundColor(style::Color::Reset))?
-                        .execute(style::Print('\n'))?
-                        .execute(cursor::MoveToColumn(0))?;
-
-                    guess.clear();
-                }
+                (KeyCode::Char(char), _) => handle_character(&wordle, &mut guess, char)?,
+                (KeyCode::Backspace, _) => handle_backspace(&mut guess)?,
+                (KeyCode::Enter, _) => { status = handle_enter(&mut wordle, &mut guess)? },
                 (_,_) => {},
             }
         }
     }
 
-    io::stdout()
-        .execute(cursor::MoveToPreviousLine(1))?
-        .execute(terminal::Clear(ClearType::CurrentLine))?
-        .execute(style::Print(format!("[done]")))?
-        .execute(cursor::MoveToNextLine(1))?;
-
     disable_raw_mode()?;
+    Ok(())
+}
+
+fn handle_character(wordle: &Wordle, guess: &mut String, char: char) -> Result<()> {
+    if guess.len() == 5 {
+        return Ok(());
+    }
+
+    guess.push(char);
+    let suggest = wordle.suggest(char, guess.len() - 1);
+    let color = match suggest {
+        Some(Hint::Green) => style::Color::DarkGreen,
+        Some(Hint::Yellow) => style::Color::Yellow,
+        Some(Hint::NotIn) => style::Color::DarkRed,
+        None => style::Color::Reset,
+    };
+
+    io::stdout()
+        .execute(style::SetForegroundColor(color))?
+        .execute(style::Print(char))?;
+    Ok(())
+}
+
+fn handle_backspace(guess: &mut String) -> Result<()> {
+    if guess.len() == 0 {
+        return Ok(());
+    }
+
+    guess.pop();
+    io::stdout()
+        .execute(cursor::MoveLeft(1))?
+        .execute(style::Print(' '))?
+        .execute(cursor::MoveLeft(1))?;
+    Ok(())
+}
+
+fn handle_enter(wordle: &mut Wordle, guess: &mut String) -> Result<Option<Status>> {
+
+    //TODO: Move this logic to wordle.rs
+    let status = 
+        if guess.len() != 5 {
+            Some(Status::InvalidLength)
+        } else if !wordle::words::VALID.contains(&guess) {
+            Some(Status::InvalidWord)
+        } else if wordle.guesses.contains(&guess) {
+            Some(Status::AlreadyGuessed)
+        } else {
+            None
+        };
+
+    if status.is_some() {
+        print_status(status.unwrap())?;
+        return Ok(status)
+    }
+
+    let result = wordle.guess(&guess);
+
+    io::stdout()
+        .execute(cursor::MoveToColumn(0))?;
+
+    for (pos, char) in guess.chars().enumerate() {
+        let hint = wordle.hint(char, pos);
+        let (fg, bg) = match hint {
+            Hint::Green => (style::Color::Black, style::Color::DarkGreen),
+            Hint::Yellow => (style::Color::Black, style::Color::DarkYellow),
+            Hint::NotIn => (style::Color::Reset, style::Color::Reset),
+        };
+        io::stdout()
+            .execute(style::SetForegroundColor(fg))?
+            .execute(style::SetBackgroundColor(bg))?
+            .execute(style::Print(char))?;
+    }
+
+    io::stdout()
+        .execute(cursor::MoveToColumn(0))?
+        .execute(style::SetForegroundColor(style::Color::Reset))?
+        .execute(style::SetBackgroundColor(style::Color::Reset))?
+        .execute(style::Print('\n'))?
+        .execute(cursor::MoveToColumn(0))?;
+
+    guess.clear();
+    Ok(None)
+}
+
+fn print_status(status: Status) -> Result<()> {
+    io::stdout()
+        .execute(cursor::SavePosition)?
+        .execute(cursor::MoveToColumn(8))?
+        .execute(style::SetForegroundColor(style::Color::Red))?
+        .execute(style::Print(status.msg()))?
+        .execute(cursor::RestorePosition)?
+        ;
+    Ok(())
+}
+
+fn clear_status() -> Result<()> {
+    io::stdout()
+        .execute(cursor::SavePosition)?
+        .execute(cursor::MoveToColumn(8))?
+        .execute(terminal::Clear(terminal::ClearType::UntilNewLine))?
+        .execute(cursor::RestorePosition)?
+        ;
     Ok(())
 }
--- src/wordle.rs
+++ src/wordle.rs
@@ -6,74 +6,75 @@ pub mod words;
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Ord, PartialOrd)]
 pub enum Hint {
-    Gray,
     Yellow,
-    Green(usize),
+    Green,
+    NotIn,
 }
-
-type Guess = Vec<(char, Hint)>;
 
 #[derive(Default)]
 pub struct Wordle {
     word: String,
-    guesses: Vec<Guess>,
+    pub guesses: Vec<String>,
 }
 
 impl Wordle {
     pub fn new(challenge_number: usize) -> Self {
-        let word = words::WORDS[challenge_number].to_owned();
+        let word = words::WORDS[challenge_number % words::WORDS_LEN].to_owned();
         Wordle { word, ..Default::default() }
     }
 
     /// Guess a string and add it to the list of guesses
-    pub fn guess(&mut self, guess: &str) -> Guess {
+    pub fn guess(&mut self, guess: &str) -> bool {
         assert_eq!(guess.len(), 5);
-
-        let solution = &self.word;
-        let guess_result = guess.chars()
-            .zip(solution.chars())
-            .enumerate()
-            .map(|(idx, (guess_char, solution_char))| {
-                let hint = 
-                    if guess_char == solution_char {
-                        Hint::Green(idx)
-                    } else if solution.contains(guess_char) {
-                        Hint::Yellow
-                    } else {
-                        Hint::Gray
-                    };
-                (guess_char, hint)
-            })
-            .collect::<Guess>();
-
-        self.guesses.push(guess_result.clone());
-        guess_result
+        if guess == &self.word {
+            true // You win!
+        } else {
+            self.guesses.push(guess.to_owned());
+            false
+        }
     }
 
-    /// Provide a hint for a single character
-    pub fn hint(&self, char: &char, pos: usize) -> Option<Hint> {
-        let mut overall_hint = None;
+    /// Provide a hint for a single character against the solution
+    pub fn hint(&self, char: char, pos: usize) -> Hint {
+        if self.word.chars().nth(pos) == Some(char) {
+            Hint::Green
+        } else if self.word.contains(char) {
+            Hint::Yellow
+        } else {
+            Hint::NotIn
+        }
+    }
 
-        let known_hints = self.guesses.iter()
-            .flatten()
-            .filter_map(|(c, hint)| (c == char).then_some(*hint));
+    /// Provide a suggestion for a single character against all known hints
+    pub fn suggest(&self, char: char, pos: usize) -> Option<Hint> {
+        if self.is_known(char, pos) {
+            Some(Hint::Green)
+        } else if self.is_seen(char) && self.word.contains(char) {
+            Some(Hint::Yellow)
+        } else if self.is_seen(char) && !self.word.contains(char) {
+            Some(Hint::NotIn)
+        } else {
+            None
+        }
+    }
 
-        for hint in known_hints.rev() {
-            if let Hint::Green(idx) = hint {
-                if idx == pos {
-                    overall_hint = Some(hint);
-                    break;
-                } else {
-                    overall_hint = Some(Hint::Yellow)
-                }
-            } else if hint == Hint::Yellow {
-                overall_hint = Some(hint);
-                break;
-            } else if hint == Hint::Gray {
-                overall_hint = Some(hint);
+    fn is_seen(&self, char: char) -> bool {
+        for guess in self.guesses.iter() {
+            if guess.contains(char) {
+                return true;
             }
         }
-
-        overall_hint
+        false
     }
+
+    fn is_known(&self, char: char, pos: usize) -> bool {
+        for guess in self.guesses.iter() {
+            if guess.chars().nth(pos) == Some(char) 
+            && self.word.chars().nth(pos) == Some(char) {
+                return true;
+            }
+        }
+        false
+    }
+
 }
